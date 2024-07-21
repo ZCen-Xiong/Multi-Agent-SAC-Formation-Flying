b40fcbb2c5338bc6110ac980fd1012720b9562e4
ZCen-Xiong <zicenxiong42@gmail.com>
2024-07-21 14:56:30 +0800

    [MPC_FF] right number of obs [env] deprecated

--- MPC_FF.py
+++ MPC_FF.py
@@ -12,20 +12,20 @@ mu = 398600.5e9
 sma = 7108e3
 nT = np.sqrt(mu/sma**3)
 # 动作维度
-n_observ = 6
+n_obs = 6
 n_state = 6
 n_input = 3
 # 预测 horizon
 ny = 35
 
 A,B = discrete_state(dT,nT)
-C = np.eye(6)
+C = np.eye(n_obs)
 R = np.eye(3) * 1e6
 Q11 = np.eye(3) * 1e1
 Q22 = np.eye(3) * 2
 Q = np.block([[Q11, np.zeros((3, 3))], [np.zeros((3, 3)), Q22]])
-S = np.eye(6)
-mpc = MPC_dep(n_state, n_observ, ny, A, B, C, R, Q, S)
+S = np.eye(n_obs)
+mpc = MPC_dep(n_state, n_obs, ny, A, B, C, R, Q, S)
 H = mpc.H_func()
 f = mpc.f_func()
 options = {'disp': False}
@@ -34,7 +34,7 @@ Xi_0 = CW.orbitgen(nT,0.1*np.pi,0.2*np.pi,200)
 Xf_0 = CW.orbitgen(nT,0.4*np.pi,0.1*np.pi,200)
 
 Xf = CW.Free(0*np.pi/nT,0,Xf_0,nT)
-x = np.zeros((6, int(np.ceil(Tf/dT))+1))            # State vector
+x = np.zeros((n_obs, int(np.ceil(Tf/dT))+1))            # State vector
 delta_u = np.zeros((3, int(np.ceil(Tf/dT))))     # Input increment vector
 
 x[:,0] = CW.Free(-0.4*np.pi/nT,0,Xi_0,nT);  
@@ -42,10 +42,10 @@ x[:,0] = CW.Free(-0.4*np.pi/nT,0,Xi_0,nT);
 dV = 0
 for t, i in zip(np.arange(0, Tf, dT) , range(0,int(np.ceil(Tf/dT)))):
     # 预测的目标状态
-    r = np.zeros(ny*6)
+    r = np.zeros(ny*n_obs)
     for ri in range(1, ny+1):
         t_r = t + (ri-1)*dT
-        r[ri*6-6:ri*6] = CW.Free(t_r, 0, Xf, nT)
+        r[ri*n_obs-n_obs:ri*n_obs] = CW.Free(t_r, 0, Xf, nT)
     
     # 推力限制
     Mbar = mpc.Mbar_func()
@@ -53,7 +53,7 @@ for t, i in zip(np.arange(0, Tf, dT) , range(0,int(np.ceil(Tf/dT)))):
     # ww = mpc.Acons_func()
     # cc = mpc.Acons_func()@ Mbar @ x[:, i].reshape(6, 1)
     # qq = mpc.Dy_func(-np.ones((6,1))*4e3, np.ones((6,1))*4e3)
-    Bcons = mpc.Dy_func(-np.ones((6,1))*4e3, np.ones((6,1))*4e3) - mpc.Acons_func()@ Mbar @ x[:, i].reshape(6, 1)
+    Bcons = mpc.Dy_func(-np.ones((n_obs,1))*4e3, np.ones((n_obs,1))*4e3) - mpc.Acons_func()@ Mbar @ x[:, i].reshape(n_obs, 1)
     lb = -np.ones([n_input*ny,1])*thrust_max
     ub = np.ones([n_input*ny,1])*thrust_max
     
@@ -66,7 +66,7 @@ for t, i in zip(np.arange(0, Tf, dT) , range(0,int(np.ceil(Tf/dT)))):
     # if exitflag == -2:
     #     break
     
-    delta_u[:, i] = delta_uFut[0:3].flatten() # 获取要应用的输入增量
+    delta_u[:, i] = delta_uFut[0:n_input].flatten() # 获取要应用的输入增量
     x[:, i+1] = A @ x[:, i] + B @ delta_u[:, i]  # 计算下一个状态以更新位置系统
     dV += np.linalg.norm(delta_u[:, i])
     # print(x[:, i+1])
